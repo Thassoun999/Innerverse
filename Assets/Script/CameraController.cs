a61136dba96596916eb8ace030bf4ee63d48fbaf
Aeva Rebelo <aevaternum@gmail.com>
2023-02-02 01:29:13 -0500

    Added prototype code for zoom function, needs review

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -4,19 +4,36 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float mouseSensitivity = 0.001f; 
+    [SerializeField] private Camera cam;
+    public float mouseSensitivity = 0.001f;
+    // last position taken on mouse button down's frame to match against new positions
     public Vector3 lastPosition;
+    [SerializeField] private float minFov = 20;
+    [SerializeField] private float maxFov = 80; 
+
     // Start is called before the first frame update
     void Start()
     {
-        lastPosition= transform.position; 
+        
+        lastPosition = transform.position;
+        // ensures that the field of view is within min and max on startup
+        UpdateFov(cam.fieldOfView);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         PanCamera();
+
+        // FOV change upon mouse scrollwheel change
+        float scrollInput = Input.GetAxis("Mouse ScrollWheel");
+        if (scrollInput > 0)
+            UpdateFov(cam.fieldOfView - 1);
+        else if (scrollInput < 0)
+            UpdateFov(cam.fieldOfView + 1);
     }
+
     void PanCamera()
     {
         // saves position of mouse in world space when drag starts (rmb) 
@@ -31,5 +48,10 @@ public class CameraController : MonoBehaviour
             transform.Translate(delta.x * mouseSensitivity, 0, delta.y * mouseSensitivity);
             lastPosition = Input.mousePosition;
         }
+    }
+
+    void UpdateFov(float newFov)
+    {
+        cam.fieldOfView = Mathf.Clamp(newFov, minFov, maxFov);
     }
 }
