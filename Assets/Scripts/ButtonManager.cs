87f9b2e3d8dbc4ea0662ab6b4e9fea69fa5f4b05
Aeva Rebelo <aevaternum@gmail.com>
2023-02-05 00:10:48 -0500

    Added clickable UI buttons

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ButtonManager : MonoBehaviour
+{
+    [SerializeField] public float alphaThreshold = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Image>().alphaHitTestMinimumThreshold = alphaThreshold;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
